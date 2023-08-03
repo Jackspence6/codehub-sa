4592455ed91641ead5bb1bdb12b312e08fbcea3e
Jackspence6 <jackspence123456@gmail.com>
2023-08-03 17:49:06 +0200

    Added external dependencies and global variables for mentorship program.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,13 +2,21 @@
 /* External dependencies */
 /******************************************/
 // Mentorship Program Variables
+var mentorBtnEl = document.getElementById("mentor-btn");
 var mentorNameEl = document.getElementsByClassName("mentor-name");
 var mentorLanguageEl = document.getElementsByClassName("mentor-language");
 var mentorCountryEl = document.getElementsByClassName("mentor-country");
+
+var mentorNameInputEl = document.getElementById("name");
+var mentorLanguageInputEl = document.getElementById("language");
+var mentorCountryInputEl = document.getElementById("country");
 /******************************************/
 /* Global variables and constants */
 /******************************************/
-
+// Mentorship Program Global variables and constants
+var mentorNameInput = mentorNameInputEl.value;
+var mentorLanguageInput = mentorLanguageInputEl.value;
+var mentorCountryInput = mentorCountryInputEl.value;
 /******************************************/
 /* Function and class declarations */
 /******************************************/
