850af3f08bebd7f26190943fdaeac09e782694f8
Jackspence6 <jackspence123456@gmail.com>
2023-08-04 14:31:01 +0200

    Added function to become a new mentor.

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,14 @@
 /* External dependencies */
 /******************************************/
 // Mentorship Program Variables
+var mentorBodyEl = document.getElementById("mentorship-body");
+var mentorCardContainerEl = document.getElementsByClassName(
+  "mentor-card-container"
+);
+var mentorFormEl = document.getElementById("mentor-form");
 var mentorBtnEl = document.getElementById("mentor-btn");
+var submitMentorBtnEl = document.getElementById("submit-mentor-btn");
+
 var mentorNameEl = document.getElementsByClassName("mentor-name");
 var mentorLanguageEl = document.getElementsByClassName("mentor-language");
 var mentorCountryEl = document.getElementsByClassName("mentor-country");
@@ -14,13 +21,58 @@ var mentorCountryInputEl = document.getElementById("country");
 /* Global variables and constants */
 /******************************************/
 // Mentorship Program Global variables and constants
-var mentorNameInput = mentorNameInputEl.value;
-var mentorLanguageInput = mentorLanguageInputEl.value;
-var mentorCountryInput = mentorCountryInputEl.value;
+
 /******************************************/
 /* Function and class declarations */
 /******************************************/
+function becomeMentor() {
+  // Create the mentor card elements
+  var mentorCardEl = document.createElement("div");
+  mentorCardEl.classList.add("row", "mentor-card");
 
+  var cols12El = document.createElement("div");
+  cols12El.classList.add("col", "s12");
+
+  var cardEl = document.createElement("div");
+  cardEl.classList.add("card");
+
+  var cardContentEl = document.createElement("div");
+  cardContentEl.classList.add("card-content", "white-text");
+
+  var mentorNameEl = document.createElement("span");
+  mentorNameEl.classList.add("card-title", "mentor-name");
+  mentorNameEl.textContent = mentorNameInputEl.value;
+
+  var mentorLanguageEl = document.createElement("span");
+  mentorLanguageEl.classList.add("mentor-language");
+  mentorLanguageEl.textContent = mentorLanguageInputEl.value;
+
+  var mentorCountryEl = document.createElement("span");
+  mentorCountryEl.classList.add("mentor-country");
+  mentorCountryEl.textContent = mentorCountryInputEl.value;
+
+  var contactEl = document.createElement("div");
+  contactEl.classList.add("card-action");
+
+  var contactBtnEl = document.createElement("a");
+
+  // Appending elements to each other
+  contactEl.appendChild(contactBtnEl);
+
+  cardContentEl.appendChild(mentorNameEl);
+  cardContentEl.appendChild(mentorLanguageEl);
+  cardContentEl.appendChild(mentorCountryEl);
+
+  cardEl.appendChild(cardContentEl);
+  cardEl.appendChild(contactEl);
+
+  cols12El.appendChild(cardEl);
+
+  mentorCardEl.appendChild(cols12El);
+
+  // Append the mentor card to the container
+  mentorCardContainerEl.appendChild(mentorCardEl);
+}
 /******************************************/
 /* Event listeners */
 /******************************************/
@@ -28,6 +80,8 @@ var mentorCountryInput = mentorCountryInputEl.value;
 $(document).ready(function () {
   $(".modal").modal();
 });
+
+submitMentorBtnEl.addEventListener("click", becomeMentor);
 /******************************************/
 /* Document manipulation */
 /******************************************/
