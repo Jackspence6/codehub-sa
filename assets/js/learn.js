84f667d2bed5f962d5fa1efdd54d62e19cb6b8c9
Jackspence6 <jackspence123456@gmail.com>
2023-08-09 13:52:13 +0200

    Added card creation function from saved data and added data to card data array.

--- assets/js/learn.js
+++ assets/js/learn.js
@@ -8,36 +8,103 @@ document.addEventListener("DOMContentLoaded", function () {
   /******************************************/
   /* Global variables and constants */
   /******************************************/
-  var answerOutputEl;
-  // Storing the selected question card as a variable
   var selectedQuestionCardEl;
   var questionInputEl;
   var questionIndex;
   var answerInputEl;
   var answerOutputEl;
-  // Initializing an empty array to save questions, answers and indexes.
   var questionsAndAnswersArray = [];
-  //  Initializing an empty object to save questions, answers and indexes.
-  var questionCardData = {
-    question: questionInputEl,
-    answer: answerInputEl,
-    index: questionIndex,
-  };
   /******************************************/
   /* Function and class declarations */
   /******************************************/
-  // function to add user question from form to card
+  // Function to add user question from form to card
   function addQuestion(event) {
     event.preventDefault();
     questionInputEl = document.getElementById("question-input").value;
-    // pushing question & index to object
-    questionsAndAnswersArray.push({
+
+    // Creating a new question object
+    var newQuestion = {
       question: questionInputEl,
       // No answer yet so assigning null to answer value
       answer: null,
       index: questionContainerEl.children.length,
-    });
+    };
 
+    // Pushing the question object to the array
+    questionsAndAnswersArray.push(newQuestion);
+
+    // Saving questions & indexes to local storage with the key, "questionCards".
+    localStorage.setItem(
+      "questionCards",
+      JSON.stringify(questionsAndAnswersArray)
+    );
+
+    // Calling the function to create and populate the question card
+    createAndPopulateQuestionCard(newQuestion);
+
+    // Resetting the form inputs after submission
+    document.getElementById("first_name").value = "";
+    document.getElementById("last_name").value = "";
+    document.getElementById("question_language").value = "";
+    document.getElementById("question-input").value = "";
+    document.getElementById("email").value = "";
+  }
+
+  // Function to handle which question is being answered
+  function handleAnswerClick(event) {
+    event.preventDefault();
+    selectedQuestionCardEl = event.target.closest(".questionCard");
+  }
+
+  // Function to add answer to back of selected question card.
+  function addAnswer(event) {
+    event.preventDefault();
+    if (selectedQuestionCardEl) {
+      answerInputEl = document.getElementById("answer-text").value;
+      // Finding the index of the question being answered
+      questionIndex = Array.from(questionContainerEl.children).indexOf(
+        selectedQuestionCardEl
+      );
+      // Assigning question answer
+      questionsAndAnswersArray[questionIndex].answer = answerInputEl;
+      // Saving answers to local storage with the key, "questionCards".
+      localStorage.setItem(
+        "questionCards",
+        JSON.stringify(questionsAndAnswersArray)
+      );
+      answerOutputEl = selectedQuestionCardEl.querySelector("#answer-output");
+      answerOutputEl.textContent = answerInputEl;
+      console.log(answerInputEl);
+      // Clearing the Answer input field after submission
+      document.getElementById("answer-text").value = "";
+      // Closing the answer modal after submission
+      $("#modal2").modal("close");
+    }
+    // Logging questionsAndAnswersArray to console
+    console.log(questionsAndAnswersArray);
+  }
+
+  // Function to generate cards from local storage data
+  function generateCardsFromSavedData() {
+    // Getting the saved card data from local storage
+    var savedDataString = localStorage.getItem("questionCards");
+    // Checking to see if there is card data saved to local storage
+    if (savedDataString) {
+      // If there is, assigning the data to the variable savedData and parsing it back from a string
+      var savedData = JSON.parse(savedDataString);
+
+      // Updating the questionsAndAnswersArray with the savedData
+      questionsAndAnswersArray = savedData;
+
+      // Looping through the saved data to create and populate cards
+      for (var i = 0; i < savedData.length; i++) {
+        createAndPopulateQuestionCard(savedData[i]);
+      }
+    }
+  }
+
+  // Function to create and populate a question card
+  function createAndPopulateQuestionCard(cardData) {
     // Creating Question and answer elements
     var questionCardEl = document.createElement("div");
     questionCardEl.classList.add("card", "questionCard");
@@ -63,7 +130,8 @@ document.addEventListener("DOMContentLoaded", function () {
     var questionOutputEl = document.createElement("p");
     questionOutputEl.setAttribute("id", "question-output");
     questionOutputEl.classList.add("white-text");
-    questionOutputEl.textContent = questionInputEl;
+    // Setting the question data from cardData
+    questionOutputEl.textContent = cardData.question;
 
     var answerRevealEl = document.createElement("div");
     answerRevealEl.classList.add("card-reveal");
@@ -79,11 +147,14 @@ document.addEventListener("DOMContentLoaded", function () {
     answerOutputEl = document.createElement("p");
     answerOutputEl.setAttribute("id", "answer-output");
     answerOutputEl.classList.add("black-text");
+    if (cardData.answer !== null) {
+      // Setting the answer data from cardData if available
+      answerOutputEl.textContent = cardData.answer;
+    }
 
     var answerQuestionBtnContainerEl = document.createElement("div");
     answerQuestionBtnContainerEl.classList.add("card-action");
 
-    // Modal Trigger
     var answerQuestionBtnEl = document.createElement("a");
     answerQuestionBtnEl.classList.add("modal-trigger");
     answerQuestionBtnEl.href = "#modal2";
@@ -109,72 +180,8 @@ document.addEventListener("DOMContentLoaded", function () {
     answerQuestionBtnContainerEl.appendChild(answerQuestionBtnEl);
 
     questionContainerEl.appendChild(questionCardEl);
-
-    // Resetting the form inputs after submission
-    document.getElementById("first_name").value = "";
-    document.getElementById("last_name").value = "";
-    document.getElementById("question_language").value = "";
-    document.getElementById("question-input").value = "";
-    document.getElementById("email").value = "";
-
-    // Saving questions & indexes to local storage with the key, "questionCards".
-    localStorage.setItem(
-      "questionCards",
-      JSON.stringify(questionsAndAnswersArray)
-    );
   }
 
-  // Function to handle which question is being answered
-  function handleAnswerClick(event) {
-    event.preventDefault();
-    // determining which question is being answered
-    selectedQuestionCardEl = event.target.closest(".questionCard");
-    console.log(selectedQuestionCardEl);
-  }
-
-  // Function to add answer to back of selected question card.
-  function addAnswer(event) {
-    event.preventDefault();
-    if (selectedQuestionCardEl) {
-      answerInputEl = document.getElementById("answer-text").value;
-      // Finding the index of the question being answered
-      questionIndex = Array.from(questionContainerEl.children).indexOf(
-        selectedQuestionCardEl
-      );
-      // Assigning question answer
-      questionsAndAnswersArray[questionIndex].answer = answerInputEl;
-      // Saving answers to local storage with the key, "questionCards".
-      localStorage.setItem(
-        "questionCards",
-        JSON.stringify(questionsAndAnswersArray)
-      );
-      answerOutputEl = selectedQuestionCardEl.querySelector("#answer-output");
-      answerOutputEl.textContent = answerInputEl;
-      console.log("Adding Answer");
-      // Clearing the Answer input field after submission
-      answerInputEl.value = "";
-      document.getElementById("answer-text").value = "";
-      // Closing the answer modal after submission
-      $("#modal2").modal("close");
-    }
-    // logging questionsAndAnswersArray to console
-    console.log(questionsAndAnswersArray);
-  }
-
-  // Function to generate cards from local storage data
-  function generateCardsFromSavedData() {
-    // Getting the saved card data from local storage
-    var savedDataString = localStorage.getItem("questionCards");
-    // checking to see if there is card data saved to local storage
-    if (savedDataString) {
-      // If there is, assigning the data to the variable savedData and parsing it back from a string
-      var savedData = JSON.parse(savedDataString);
-    }
-    // Logging the saved data to the console
-    console.log(savedData);
-  }
-  // Calling the generateCardsFromSavedData function on page load
-  generateCardsFromSavedData();
   /******************************************/
   /* Event listeners */
   /******************************************/
@@ -190,13 +197,10 @@ document.addEventListener("DOMContentLoaded", function () {
   // Event to open modal on click
   $("#modal2").modal();
   /******************************************/
-  /* Document manipulation */
-  /******************************************/
-
-  /******************************************/
   /* Initialization code */
   /******************************************/
-
+  // Calling the generateCardsFromSavedData function on page load to generate cards that were previously created
+  generateCardsFromSavedData();
   /******************************************/
   /* Main logic */
   /******************************************/
