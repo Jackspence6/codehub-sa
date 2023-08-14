49fcfec8a73a21a382215911532f25ac13ecb1c1
Jackspence6 <jackspence123456@gmail.com>
2023-08-14 15:03:33 +0200

    Fixed challenge search function.

--- assets/js/challenges.js
+++ assets/js/challenges.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", function () {
   /* External dependencies */
   /******************************************/
   var topicsEl = document.getElementsByClassName("topics");
+  var searchEl = document.getElementById("search");
+  var challengeCards = document.querySelectorAll(".challenge-card");
   /******************************************/
   /* Global variables and constants */
   /******************************************/
@@ -10,17 +12,36 @@ document.addEventListener("DOMContentLoaded", function () {
   /******************************************/
   /* Function and class declarations */
   /******************************************/
-  function topicList() {
-    // Looping through the card topics and logging their text content
-    for (var i = 0; i < topicsEl.length; i++) {
-      console.log(topicsEl[i].textContent);
+  // Function to search for challenge card based on challenge topics
+  function searchChallenges() {
+    // Converting search input to lowercase for comparison
+    var searchInput = searchEl.value.toLowerCase();
+
+    // looping through challenge cards
+    for (var i = 0; i < challengeCards.length; i++) {
+      var challengeCard = challengeCards[i];
+      var challengeTopics = challengeCard.querySelectorAll(".topics");
+      var shouldDisplay = false;
+
+      // Checking if any challenge topic matches the user search input
+      for (var j = 0; j < challengeTopics.length; j++) {
+        if (
+          challengeTopics[j].textContent.toLowerCase().includes(searchInput)
+        ) {
+          shouldDisplay = true;
+          break;
+        }
+      }
+
+      // Setting the display property of the card based on the shouldDisplay variable
+      challengeCard.style.display = shouldDisplay ? "block" : "none";
     }
   }
-  topicList();
   /******************************************/
   /* Event listeners */
   /******************************************/
-
+  //   Event Listener to activate the searchChallenges function when a user starts typing
+  searchEl.addEventListener("input", searchChallenges);
   /******************************************/
   /* Document manipulation */
   /******************************************/
